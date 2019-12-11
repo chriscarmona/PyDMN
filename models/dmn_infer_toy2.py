abb9f9c21f0fe56bcfb5a8d9281fbca27ce5a9d0
Christian Carmona <carmona@stats.ox.ac.uk>
2019-12-11 18:26:59 +0000

    Directed and Weighted networks supported

--- models/dmn_infer_toy2.py
+++ models/dmn_infer_toy2.py
@@ -30,34 +30,54 @@ class dmn_toy2( pyro.nn.PyroModule ):
 
     """
 
-    def __init__( self, edgelist, directed=False, H_dim=3, random_kernel=False, jitter=1e-3, init_mcmc=False ):
+    def __init__( self, edgelist, directed=False, weighted=False, H_dim=3, sigma_k_prior_param=None, random_kernel=False, jitter=1e-3, init_mcmc=False ):
 
         super(dmn_toy2, self).__init__()
 
-        self.weighted = False
         self.directed = directed
+        self.weighted = weighted
         self.coord = True
         self.socpop = False
 
-        Y, [all_nodes, Y_time, all_layers] = pydmn.util.edgelist_to_tensor( edgelist = edgelist )
+        self.edgelist = edgelist
+        Y, [all_nodes, Y_time, all_layers] = pydmn.util.edgelist_to_tensor( edgelist = self.edgelist )
+
+        Y = torch.tensor(Y)
+        Y_time = torch.tensor(Y_time)
         self.all_layers = all_layers
-        
+        self.all_nodes = all_nodes
+
         self.set_data(Y=Y, Y_time=Y_time, H_dim=H_dim )
 
         self.random_kernel = random_kernel
         self.jitter = jitter
 
+        # Number of directions to be considered
+        # 2 if the network is directed: Send & receive
+        # 1 if not
         if self.directed:
             self.n_dir = 2
         else:
             self.n_dir = 1
 
+
+        # We consider one set of GPs for the propensity of connection
+        # and (for weighted networks) one more set for the strenght of that connection
+        if self.weighted:
+            self.n_w = 2
+            self.sigma_k_prior_param = torch.tensor([11.,20.]) if sigma_k_prior_param is None else sigma_k_prior_param
+        else:
+            self.n_w = 1
+
         ### Variational Parameters ###
-        self.gp_mean_param = torch.ones((self.V_net,self.H_dim,self.n_dir,2))
-        self.gp_coord_demean = torch.zeros((self.V_net,self.H_dim,self.T_net,self.n_dir))
-        self.gp_cov_tril = torch.eye(self.T_net).expand((self.V_net,self.H_dim,self.n_dir,self.T_net,self.T_net))
+        # To be set inside the guide
+        self.gp_mean_loc = None
+        self.gp_mean_scale = None
+        self.gp_coord_demean = None
+        self.gp_cov_tril = None
 
         if self.random_kernel:
+            # If we optimise the parameters in the kernel, they will be stored here
             self.kernel_param = torch.ones((2,2))
 
         # If the Kernel IS random, we use PyroSample
@@ -80,8 +100,8 @@ class dmn_toy2( pyro.nn.PyroModule ):
         if init_mcmc:
             self.gp_mean_ini, self.gp_coord_demean_ini = self.init_guide()
         else:
-            self.gp_mean_ini = torch.zeros( (self.V_net,self.H_dim,self.n_dir) )
-            self.gp_coord_demean_ini = torch.zeros( (self.V_net,self.H_dim,self.n_dir, self.T_net) )
+            self.gp_mean_ini = torch.zeros( (self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w) )
+            self.gp_coord_demean_ini = torch.zeros( (self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w,self.T_net) )
 
     def model(self):
 
@@ -95,32 +115,44 @@ class dmn_toy2( pyro.nn.PyroModule ):
         # Lff = Kff.cholesky() # cholesky lower triangular
         Lff = self.Lff_ini
         # Sampling latent coordinates #
-        with pyro.plate('gp_coord_all', self.V_net*self.H_dim*self.n_dir ):
+        with pyro.plate('gp_coord_all', self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w ):
             # Mean function of the GPs
             gp_mean = pyro.sample( "gp_mean",
-                                    dist.Normal( torch.zeros( (self.V_net*self.H_dim*self.n_dir) ),
+                                    dist.Normal( torch.zeros( (self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w) ),
                                                 torch.tensor([0.1]) ) )
             # Demeaned GPs
             gp_coord_demean = pyro.sample( "gp_coord_demean",
-                                            dist.MultivariateNormal( torch.zeros( (self.V_net*self.H_dim*self.n_dir, self.T_net) ),
+                                            dist.MultivariateNormal( torch.zeros( (self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w, self.T_net) ),
                                                                         scale_tril=Lff ) )
 
-        gp_mean = gp_mean.reshape(self.V_net,self.H_dim,self.n_dir)
-        gp_coord_demean = gp_coord_demean.reshape(self.V_net, self.H_dim, self.n_dir, self.T_net)
+        gp_mean = gp_mean.reshape(self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w)
+        gp_coord_demean = gp_coord_demean.reshape(self.V_net, self.H_dim, self.K_net, self.n_dir, self.n_w, self.T_net)
 
         # Latent coordinates
-        gp_coord = gp_mean.expand(self.T_net, self.V_net, self.H_dim, self.n_dir).permute(1,2,3,0) + gp_coord_demean
+        gp_coord = gp_mean.expand(self.T_net, self.V_net, self.H_dim, self.K_net, self.n_dir, self.n_w).permute(1,2,3,4,5,0) + gp_coord_demean
 
         ### Linear Predictor ###
-        Y_linpred = torch.einsum('vht,wht->vwt', gp_coord[:,:,0,:], gp_coord[:,:,self.n_dir-1,:])
+        Y_linpred = torch.einsum('uhkwt,vhkwt->uvtkw', gp_coord[:,:,:,0,:,:], gp_coord[:,:,:,self.n_dir-1,:,:])
 
-        ### Link probability ###
-        Y_link_prob = torch.sigmoid(Y_linpred)
+        ### Link propensity (probability of occur) ###
+        Y_link_prob = torch.sigmoid(Y_linpred[:,:,:,:,0])
         Y_link_prob_valid = Y_link_prob.flatten()[self.Y_valid_id.flatten()==1]
-        # plt.scatter(self.Y_valid_obs,Y_link_prob_valid.detach())
 
-        with pyro.plate( "data", self.Y_valid_obs.shape[0]):
-            pyro.sample( "obs", dist.Bernoulli(Y_link_prob_valid), obs=self.Y_valid_obs )
+        with pyro.plate( "data", Y_link_prob_valid.shape[0]):
+            pyro.sample( "obs", dist.Bernoulli(Y_link_prob_valid), obs=self.Y_link.flatten()[self.cond_Y_link] )
+
+        ### Link expected weight (weight given occurrence) ###
+        if self.weighted:
+            with pyro.plate( "sigma_k_ind", self.K_net):
+                sigma_k = pyro.sample( 'sigma_k', dist.InverseGamma( self.sigma_k_prior_param[0].expand(self.K_net), self.sigma_k_prior_param[1].expand(self.K_net) ) )
+            Y_link_SDw = sigma_k.expand(self.V_net,self.V_net,self.T_net,self.K_net)
+
+            Y_link_Ew = Y_linpred[:,:,:,:,1]
+            # cond_Y_w: condition of being positive and valid weights (defined in set_data())
+            Y_link_Ew_valid = Y_link_Ew.flatten()[self.cond_Y_w]
+            Y_link_SDw_valid = Y_link_SDw.flatten()[self.cond_Y_w]
+            with pyro.plate( "data_w", Y_link_Ew_valid.shape[0] ):
+                pyro.sample( "obs_w", dist.Normal(Y_link_Ew_valid,Y_link_SDw_valid), obs=self.Y.flatten()[self.cond_Y_w] )
 
     def guide(self):
 
@@ -132,18 +164,27 @@ class dmn_toy2( pyro.nn.PyroModule ):
 
         # Sampling coordinates #
         self.gp_mean_loc = pyro.param("gp_mean_loc", self.gp_mean_ini )
-        self.gp_mean_scale = pyro.param("gp_mean_scale", 0.1*torch.ones((self.V_net,self.H_dim,self.n_dir)), constraint=constraints.positive)
+        self.gp_mean_scale = pyro.param("gp_mean_scale", 0.1*torch.ones((self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w)), constraint=constraints.positive)
         self.gp_coord_demean = pyro.param( f"gp_coord_demean_loc", self.gp_coord_demean_ini )
         # Posterior Covariance of the GP
-        self.gp_cov_tril = pyro.param( "gp_cov_tril", self.Lff_ini.expand(self.V_net,self.H_dim,self.n_dir,self.T_net,self.T_net),
+        self.gp_cov_tril = pyro.param( "gp_cov_tril", self.Lff_ini.expand(self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w,self.T_net,self.T_net),
                                         constraint=constraints.lower_cholesky )
-        with pyro.plate('gp_coord_all', self.V_net*self.H_dim*self.n_dir ):
+        with pyro.plate('gp_coord_all', self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w ):
             # Posterior GP (mean function params) #
-            pyro.sample( "gp_mean", dist.Normal( self.gp_mean_loc.reshape(self.V_net*self.H_dim*self.n_dir), self.gp_mean_scale.reshape(self.V_net*self.H_dim*self.n_dir) ) )
+            pyro.sample( "gp_mean", dist.Normal( self.gp_mean_loc.reshape(self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w),
+                                                self.gp_mean_scale.reshape(self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w) ) )
             # Posterior GP (demeaned) #
             pyro.sample( f"gp_coord_demean",
-                                    dist.MultivariateNormal( self.gp_coord_demean.reshape(self.V_net * self.H_dim * self.n_dir , self.T_net),
-                                                            scale_tril=self.gp_cov_tril.reshape(self.V_net * self.H_dim * self.n_dir , self.T_net, self.T_net) ) )
+                                    dist.MultivariateNormal( self.gp_coord_demean.reshape(self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w , self.T_net),
+                                                            scale_tril=self.gp_cov_tril.reshape(self.V_net*self.H_dim*self.K_net*self.n_dir*self.n_w , self.T_net, self.T_net) ) )
+
+        pyro.sample( "kernel.variance", dist.InverseGamma( self.kernel_param[1,0], self.kernel_param[1,1] ) )
+
+        if self.weighted:
+            self.sigma_k_post_loc = pyro.param("sigma_k_post_loc", torch.ones([1]), constraint=constraints.positive )
+            self.sigma_k_post_scale = pyro.param("sigma_k_post_scale", torch.ones([1]), constraint=constraints.positive )
+            with pyro.plate( "sigma_k_ind", self.K_net):
+                sigma_k = pyro.sample( 'sigma_k', dist.InverseGamma( self.sigma_k_post_loc, self.sigma_k_post_scale ) )
 
 
     def set_data(self, Y, Y_time, H_dim=3):
@@ -158,18 +199,20 @@ class dmn_toy2( pyro.nn.PyroModule ):
         assert Y.shape[0]==Y.shape[1]
 
         self.Y = Y
-        self.V_net, self.T_net = self.Y.shape[0], self.Y.shape[2]
+        self.V_net, self.T_net, self.K_net = self.Y.shape[0], self.Y.shape[2], self.Y.shape[3]
 
         # identifies elements of Y which will be considered for the likelihood
         if self.directed:
-            aux=torch.eye(self.V_net).expand(self.T_net,self.V_net,self.V_net).permute(1,2,0)
+            aux=torch.eye(self.V_net).expand(self.K_net,self.T_net,self.V_net,self.V_net).permute(3,2,1,0)
             self.Y_valid_id = torch.where( aux==0, torch.ones_like(Y), torch.zeros_like(Y) )
         else:
-            self.Y_valid_id = torch.tril(torch.ones((self.V_net,self.V_net)),-1).expand(self.T_net,self.V_net,self.V_net).permute(1,2,0)
-
-        self.Y_valid_obs = self.Y.flatten()[self.Y_valid_id.flatten()==1]
+            self.Y_valid_id = torch.tril(torch.ones((self.V_net,self.V_net)),-1).expand(self.K_net,self.T_net,self.V_net,self.V_net).permute(3,2,1,0)
 
         self.Y_link = torch.where( Y!=0, torch.ones_like(Y), torch.zeros_like(Y) )
+        self.cond_Y_link = self.Y_valid_id.flatten()==1
+
+        if self.weighted:
+            self.cond_Y_w = (self.Y_valid_id.flatten()==1)&(self.Y_link.flatten()==1)
 
         self.K_net = Y.shape[3] if len(Y.shape)==4 else 1
 
@@ -225,8 +268,8 @@ class dmn_toy2( pyro.nn.PyroModule ):
         hmc_samples = {k: v.detach().cpu().numpy() for k, v in mcmc.get_samples().items()}
 
         # Get latent coordinates samples from mcmc #
-        gp_mean = torch.tensor(hmc_samples['gp_mean']).reshape(num_samples,self.V_net,self.H_dim,self.n_dir)
-        gp_coord_demean = torch.tensor(hmc_samples[f'gp_coord_demean']).reshape(num_samples,self.V_net, self.H_dim, self.n_dir, self.T_net)
+        gp_mean = torch.tensor(hmc_samples['gp_mean']).reshape(num_samples,self.V_net,self.H_dim,self.K_net,self.n_dir,self.n_w)
+        gp_coord_demean = torch.tensor(hmc_samples[f'gp_coord_demean']).reshape(num_samples,self.V_net, self.H_dim,self.K_net, self.n_dir,self.n_w, self.T_net)
         # keep only last sample
         gp_mean_ini = gp_mean[num_samples-1]
         gp_coord_demean_ini = gp_coord_demean[num_samples-1]
